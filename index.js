12a981ca089e728e8071429f1563001f102f54c4
Paul Fellows <paul99fellows@gmail.com>
2024-01-30 15:09:11 +0000

    Added files.

---
+++ index.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+import Papa from 'papaparse';
+import QRCode from 'qrcode'
+
+const input = fs.createReadStream("./input/A.csv");
+
+Papa.parse(input, {
+    header: true,
+    skipEmptyLines: true,
+    complete: (results, file) => {
+        results.data.forEach((row) => {
+            QRCode.toFile(`./output/${row.Label}.png`, row.Resource, {});
+        })
+    }
+});
